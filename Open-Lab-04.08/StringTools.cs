abc4d7bc3411bc7ba5aaa46ac0c5375342ab3974
sebax774 <bugansebastian@gmail.com>
2020-10-31 02:13:51 +0100

    vypracovanie

--- Open-Lab-04.08/StringTools.cs
+++ Open-Lab-04.08/StringTools.cs
@@ -1,19 +1,23 @@
 ﻿using System;
-using System.Globalization;
 using System.Collections.Generic;
-
 namespace Open_Lab_04._08
 {
     public class StringTools
     {
         public string[] IsFourLetters(string[] strings)
         {
-            var retl = new List<string>();
+            List<string> q = new List<string>();
+
             for (int i = 0; i < strings.Length; i++)
-                if (strings[i].Length == 4)
-                    retl.Add(strings[i]);
-            string[] reta = retl.ToArray();
-            return reta;
+            {
+                if (strings[i].Length == 4) 
+                {
+                    q.Add(strings[i]);
+                    
+                }
+                    
+            }
+            return q.ToArray();
         }
     }
 }
